f7686dd724b7730699b55a179aea7c4117de5c5b
Álvaro Fidalgo <dmj200@gmail.com>
2016-11-09 18:57:14 +0100

    flatMap and getOrElse

--- src/main/scala/errors/NewOption.scala
+++ src/main/scala/errors/NewOption.scala
@@ -4,12 +4,26 @@ package errors
 trait MyOption[+A] {
 
     def map[B](f:A=>B):MyOption[B]
+
+    def flatMap[B](f:A=>MyOption[B]):MyOption[B]
+
+    def getOrElse[B >: A](default : => B) : B
 }
 
 case class Some[A](get:A) extends MyOption[A] {
+
    def map[B](f:A=>B):MyOption[B] = Some(f(get))
+
+   def flatMap[B](f:A=>MyOption[B]):MyOption[B] = f(get)
+
+   def getOrElse[B >: A](default : => B) : B = get
 }
 
 object None extends MyOption[Nothing] {
+
   def map[B](f:Nothing=>B):MyOption[B] = None
+
+  def flatMap[B](f:Nothing=>MyOption[B]):MyOption[B] = None
+
+  def getOrElse[B](default : => B) : B  = default
 }
--- src/test/scala/errors/SomeTest.scala
+++ src/test/scala/errors/SomeTest.scala
@@ -5,14 +5,27 @@ import org.scalatest.ShouldMatchers
 
 class SomeTest extends FlatSpec with ShouldMatchers{
 
-  " We want to implement map in MyOption and result " should " be None if not have elments " in {
-    val none = Some(None)
+  " We want to implement map in Some and result " should " be new Some that apply function in elements  " in {
     val value = 3
-    none.map((a)=>value) shouldBe Some(value)
+    val option = Some(2)
+    option.map((a)=>value) shouldBe Some(value)
   }
 
-  it should " be new Option that apply function in elements " in {
-    val some = Some(3)
-    some.map((a)=>s"$a") shouldBe Some("3")
+  " We want to implement flatMap in Some and result " should " be new Some when not deep " in {
+    val value = 3
+    val option = Some(2)
+    option.flatMap((a)=>Some(value)) shouldBe Some(value)
+  }
+
+  it should " be new Some without deep when exist deep " in {
+    val value = 3
+    val option = Some(Some(2))
+    option.flatMap((a)=>Some(value)) shouldBe Some(value)
+  }
+
+  " We want to implement getOrElse in Some and result " should " be value of get " in {
+    val value = 3
+    val option = Some(value)
+    option.getOrElse("assasasa") shouldBe  value
   }
 }

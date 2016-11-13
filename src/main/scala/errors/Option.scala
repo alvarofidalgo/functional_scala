6447fedc5031e10448014f58e5d6ae71788e2d83
Álvaro Fidalgo <dmj200@gmail.com>
2016-11-13 16:06:36 +0100

    remove compose function

--- src/main/scala/errors/Option.scala
+++ src/main/scala/errors/Option.scala
@@ -2,18 +2,18 @@ package errors
 
 case class Option[A](get: A) extends MyOption[A] {
 
-
-  def compose[B,C](f1:(A)=>C,f2:((A)=>C)=> B): B = f2(f1)
-
-  private def getValue [B](default:B,notDefault:B):B = get match {
+  private def getValue[B, C](default: B, fParameter: (A) => C, fCompose: ((A) => C) => B): B = get match {
     case null => default
-    case a =>notDefault
+    case a => fCompose(fParameter)
   }
 
-  override def map[B](f: (A) => B): MyOption[B] = getValue(None,compose(f,Some(get).map[B]))
+  override def map[B](f: (A) => B): MyOption[B] = getValue(None, f, Some(get).map[B])
 
+  override def flatMap[B](f: (A) => MyOption[B]): MyOption[B] = getValue(None, f, Some(get).flatMap[B])
 
-  override def flatMap[B](f: (A) => MyOption[B]): MyOption[B] = getValue(None,compose(f,Some(get).flatMap[B]))
-
-  override def getOrElse[B >: A](default: => B): B = getValue(default,get)
+  override def getOrElse[B >: A](default: => B): B = getValue[B, B](default, (a) => {
+    get
+  }, (a) => {
+    get
+  })
 }

b38f8ed0b8920a7468173e89da155caebde6d102
Álvaro Fidalgo <dmj200@gmail.com>
2016-11-13 17:03:50 +0100

    remove DRY with Some

--- src/main/scala/errors/Option.scala
+++ src/main/scala/errors/Option.scala
@@ -2,21 +2,20 @@ package errors
 
 case class Option[A](get: A) extends MyOption[A] {
 
-  private def getValue[B, C](fDefault: () => B, f: (A) => B): B = get match {
+  private def getValue[B](fDefault: () => B, f: (MyOption[A]) => B): B = get match {
     case null => fDefault()
-    case value => f(value)
+    case value => f(Some(value))
   }
 
   def map[B](f: (A) => B): MyOption[B] =
-
-    getValue(() => None.map(f), (get) => Some(get).map(f))
+    getValue(() => None.map(f), (get) => get.map(f))
 
   def flatMap[B](f: (A) => MyOption[B]): MyOption[B] =
-    getValue(() => None.flatMap(f), (get) => Some(get).flatMap(f))
+    getValue(() => None.flatMap(f), (get) => get.flatMap(f))
 
   def getOrElse[B >: A](default: => B): B =
-    getValue(() => None.getOrElse(default), (get) => Some(get).getOrElse(default))
+    getValue(() => None.getOrElse(default), (get) => get.getOrElse(default))
 
   def orElse[B >: A](default: => MyOption[B]): MyOption[B] =
-    getValue(() => None.orElse(default), (get) => Some(get).orElse(default))
+    getValue(() => None.orElse(default), (get) => get.orElse(default))
 }

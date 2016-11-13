95cef9e48bb4584c03fb458348ef9b7a5a1f96e4
Álvaro Fidalgo <dmj200@gmail.com>
2016-11-13 14:14:08 +0100

    rename functions

--- src/main/scala/errors/Option.scala
+++ src/main/scala/errors/Option.scala
@@ -3,17 +3,17 @@ package errors
 case class Option[A](get: A) extends MyOption[A] {
 
 
-  def myFunction[B,C](f1:(A)=>C,f2:((A)=>C)=> B): B = f2(f1)
+  def compose[B,C](f1:(A)=>C,f2:((A)=>C)=> B): B = f2(f1)
 
-  private def otherFunction [B](default:B,notDefault:B):B = get match {
+  private def getValue [B](default:B,notDefault:B):B = get match {
     case null => default
     case a =>notDefault
   }
 
-  override def map[B](f: (A) => B): MyOption[B] = otherFunction(None,myFunction(f,Some(get).map[B]))
+  override def map[B](f: (A) => B): MyOption[B] = getValue(None,compose(f,Some(get).map[B]))
 
 
-  override def flatMap[B](f: (A) => MyOption[B]): MyOption[B] = otherFunction(None,myFunction(f,Some(get).flatMap[B]))
+  override def flatMap[B](f: (A) => MyOption[B]): MyOption[B] = getValue(None,compose(f,Some(get).flatMap[B]))
 
-  override def getOrElse[B >: A](default: => B): B = otherFunction(default,get)
+  override def getOrElse[B >: A](default: => B): B = getValue(default,get)
 }

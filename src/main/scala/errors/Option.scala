a1b05990933fe1dd504e9134e6abc8d8b7588be3
Álvaro Fidalgo <dmj200@gmail.com>
2016-11-13 13:24:05 +0100

    generalice map and flatmap

--- src/main/scala/errors/Option.scala
+++ src/main/scala/errors/Option.scala
@@ -1,17 +1,19 @@
 package errors
 
-//TODO : DELETE DRY IN CASE NULL
-case class Option [A](get:A) extends MyOption[A]{
+//TODO : IN REFACTOR
+case class Option[A](get: A) extends MyOption[A] {
+
+
+  def myFunction[B,C](f1:(A)=>C,f2:((A)=>C)=> MyOption[B]) = f2(f1)
 
   override def map[B](f: (A) => B): MyOption[B] = get match {
     case null => None
-    case a => Some(get).map(f)
+    case a =>myFunction(f,Some(get).map[B])
   }
-
 
   override def flatMap[B](f: (A) => MyOption[B]): MyOption[B] = get match {
     case null => None
-    case a => Some(get).flatMap(f)
+    case a => myFunction(f,Some(get).flatMap[B])
   }
 
   override def getOrElse[B >: A](default: => B): B = get match {

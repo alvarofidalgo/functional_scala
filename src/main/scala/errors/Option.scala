c95f4eec01da830aa51b5d7076e15645b1a8b771
Álvaro Fidalgo <dmj200@gmail.com>
2016-11-13 13:57:03 +0100

    delete DRY with pattern matching

--- src/main/scala/errors/Option.scala
+++ src/main/scala/errors/Option.scala
@@ -4,17 +4,17 @@ package errors
 case class Option[A](get: A) extends MyOption[A] {
 
 
-  def myFunction[B,C](f1:(A)=>C,f2:((A)=>C)=> MyOption[B]) = f2(f1)
+  def myFunction[B,C](f1:(A)=>C,f2:((A)=>C)=> MyOption[B]): MyOption[B] = f2(f1)
 
-  override def map[B](f: (A) => B): MyOption[B] = get match {
+  private def otherFunction [B,C,D](default:MyOption[B],notDefuaul:MyOption[B]) = get match {
     case null => None
-    case a =>myFunction(f,Some(get).map[B])
+    case a =>notDefuaul
   }
 
-  override def flatMap[B](f: (A) => MyOption[B]): MyOption[B] = get match {
-    case null => None
-    case a => myFunction(f,Some(get).flatMap[B])
-  }
+  override def map[B](f: (A) => B): MyOption[B] = otherFunction(None,myFunction(f,Some(get).map[B]))
+
+
+  override def flatMap[B](f: (A) => MyOption[B]): MyOption[B] = otherFunction(None,myFunction(f,Some(get).flatMap[B]))
 
   override def getOrElse[B >: A](default: => B): B = get match {
     case null => default

2bfaf6f9d776a0757436b641410f381c1a71dd4a
Álvaro Fidalgo <dmj200@gmail.com>
2016-11-13 16:19:46 +0100

    unify compose funtion

--- src/main/scala/errors/Option.scala
+++ src/main/scala/errors/Option.scala
@@ -2,18 +2,14 @@ package errors
 
 case class Option[A](get: A) extends MyOption[A] {
 
-  private def getValue[B, C](default: B, fParameter: (A) => C, fCompose: ((A) => C) => B): B = get match {
+  private def getValue[B, C](default: B, f: (A) => B): B = get match {
     case null => default
-    case a => fCompose(fParameter)
+    case value => f(value)
   }
 
-  override def map[B](f: (A) => B): MyOption[B] = getValue(None, f, Some(get).map[B])
+  override def map[B](f: (A) => B): MyOption[B] = getValue(None, (get) => Some(get).map(f))
 
-  override def flatMap[B](f: (A) => MyOption[B]): MyOption[B] = getValue(None, f, Some(get).flatMap[B])
+  override def flatMap[B](f: (A) => MyOption[B]): MyOption[B] = getValue(None, (get) => Some(get).flatMap(f))
 
-  override def getOrElse[B >: A](default: => B): B = getValue[B, B](default, (a) => {
-    get
-  }, (a) => {
-    get
-  })
+  override def getOrElse[B >: A](default: => B): B = getValue(default, (get) => get)
 }

1c8effff0ab4dbc774266ff1d8ea8c3ebf12cb88
Álvaro Fidalgo <dmj200@gmail.com>
2016-11-13 17:08:21 +0100

    remove default function

--- src/main/scala/errors/Option.scala
+++ src/main/scala/errors/Option.scala
@@ -2,20 +2,20 @@ package errors
 
 case class Option[A](get: A) extends MyOption[A] {
 
-  private def getValue[B](fDefault: () => B, f: (MyOption[A]) => B): B = get match {
-    case null => fDefault()
+  private def getValue[B](f: (MyOption[A]) => B): B = get match {
+    case null => f(None)
     case value => f(Some(value))
   }
 
   def map[B](f: (A) => B): MyOption[B] =
-    getValue(() => None.map(f), (get) => get.map(f))
+    getValue((get) => get.map(f))
 
   def flatMap[B](f: (A) => MyOption[B]): MyOption[B] =
-    getValue(() => None.flatMap(f), (get) => get.flatMap(f))
+    getValue((get) => get.flatMap(f))
 
   def getOrElse[B >: A](default: => B): B =
-    getValue(() => None.getOrElse(default), (get) => get.getOrElse(default))
+    getValue((get) => get.getOrElse(default))
 
   def orElse[B >: A](default: => MyOption[B]): MyOption[B] =
-    getValue(() => None.orElse(default), (get) => get.orElse(default))
+    getValue((get) => get.orElse(default))
 }

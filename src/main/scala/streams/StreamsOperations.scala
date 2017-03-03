24cf33bf5879d65f2bb99f1994b4b1074f487cb5
Álvaro Fidalgo <dmj200@gmail.com>
2017-03-03 19:26:45 +0100

    refactor streams toList

--- src/main/scala/streams/StreamsOperations.scala
+++ src/main/scala/streams/StreamsOperations.scala
@@ -10,26 +10,27 @@ import lists.MyList
 object StreamsOperations {
 
   import lists.Nil
-  import conversions.List.recursive
+
 
   implicit class StreamsOperations[A](streams: Streams[A]) {
 
-    @tailrec
-    final def toList(implicit acc:MyList[A]=Nil,
-                              tail:Streams[A]=streams): MyList[A] = tail match {
-      case Empty => acc.reverse(Nil)
-      case InitStreams(fHead,fTail) =>
-        val myList = Init[A](fHead(),acc)
-        toList(myList,fTail())
+    import conversions.List.withFoldLeft
 
+
+    @tailrec
+    final def toList(implicit result: MyList[A] = Nil): MyList[A] = streams match {
+      case Empty => result
+      case InitStreams(head, tail) =>
+        new StreamsOperations(tail()).toList(result.append(MyList(head())))
     }
 
-    final def take(elements:Int)(implicit result:Streams[A]=Empty):Streams[A] = streams match {
-      case Empty =>  result
-      case InitStreams(fHead,fTail)  => if (elements==0)
-                                            result
-                                        else new StreamsOperations[A](fTail()).take(elements-1)(InitStreams(fHead,()
-      =>result))
+
+
+    @tailrec
+    final def take(elements:Int)(implicit result:Streams[A]=Empty):Streams[A] = (elements,streams) match {
+      case (_,Empty) =>  result
+      case (0,InitStreams(fHead,fTail))  => result
+      case (_,InitStreams(fHead,fTail)) =>new StreamsOperations[A](fTail()).take(elements-1)(InitStreams(fHead,() =>result))
 
     }
 

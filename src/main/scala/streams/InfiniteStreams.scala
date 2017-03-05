4de31c1f23177630cfc6fb755ff2a9c9780bf941
Álvaro Fidalgo <dmj200@gmail.com>
2017-03-05 20:22:24 +0100

    remove tailrec

--- src/main/scala/streams/InfiniteStreams.scala
+++ src/main/scala/streams/InfiniteStreams.scala
@@ -1,16 +1,13 @@
 package streams
 
-
 object InfiniteStreams {
 
   implicit class InfiniteStreams[A](value: A) {
 
     final def constant(implicit res: Streams[A] = Streams.cons[A](value, value.constant)): Streams[A] = res
 
+    final def from(implicit acc : Streams[Int] =  Streams.cons[Int](addOne,addOne.from)): Streams[Int] = acc
 
-    final def from: Streams[Int] = {
-      Streams.cons[Int](addOne, addOne.from)
-    }
 
     private def addOne:Int = value.asInstanceOf[Int] + 1
   }

9ddbb3f3a1672fa480e20f993ae7567b037150b1
alvarofidalgo <dmj200@gmail.com>
2017-03-11 08:46:03 +0100

    delete first else

--- src/main/scala/ramdoms/RandomGenerator.scala
+++ src/main/scala/ramdoms/RandomGenerator.scala
@@ -11,10 +11,8 @@ trait RandomGenerator {
     val min = limits._1
     val max = limits._2
     val value = nextInt._1
-    if (value - min == max + 1)
-      (0,nextInt._2)
-    else if (value - min == max +2)
-      (1,nextInt._2)
+    if (value - min > max )
+      (value - min - max - 1,nextInt._2)
     else
     (value - min,nextInt._2)
   }

d435fbca65cb6c240d00da3f698b4764ee91e054
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2017-03-12 14:01:19 +0100

    remove checke[Int]

--- src/test/scala/ramdoms/RandomGeneratorTest.scala
+++ src/test/scala/ramdoms/RandomGeneratorTest.scala
@@ -31,48 +31,64 @@ class RandomGeneratorTest extends FlatSpec with ShouldMatchers{
 
   }
 
+  trait CheckerInstance {
+
+    def apply[A](implicit checker: Checker[A]): Checker[A] = checker
+
+    implicit val nonNegative = new Checker[Int] {
+      override def functionToExecute: (Int) => (Int, RandomGenerator) =
+                        (a) => buildRandomize(addToMinValue=a).nonNegativeInt
+    }
+
+    implicit val doubleRandom = new Checker[Double] {
+      override def functionToExecute: (Int) => (Double, RandomGenerator) =
+                       (a) => buildRandomize(addToMinValue=a).doubleRandom
+    }
 
 
-  "We want to generate Random number and result " should " be Zero when nextValue return MinValue" in  new Checker[Int] {
-    override def functionToExecute: (Int) => (Int, RandomGenerator) = (a) => buildRandomize(addToMinValue=a).nonNegativeInt
-    test(addMinValue = 0,result=0)
+    implicit val intDoubleRandom = new Checker[(Int,Double)] {
+      override def functionToExecute: (Int) => ((Int, Double), RandomGenerator) = (a)=> buildRandomize(addToMinValue=a).intDoubleRandom
+    }
 
   }
 
-  it should " be one when NextInt return next MinValue " in  new Checker[Int]  {
-    override def functionToExecute: (Int) => (Int, RandomGenerator) = (a) => buildRandomize(addToMinValue=a).nonNegativeInt
-    test(addMinValue = 1,result=1)
+  object Checker1 extends  CheckerInstance
+
+
+  import  Checker1._
+
+  "We want to generate Random number and result " should " be Zero when nextValue return MinValue" in   {
+
+    nonNegative.test(addMinValue = 0,result=0)
+
+  }
+
+  it should " be one when NextInt return next MinValue " in   {
+    nonNegative.test(addMinValue = 1,result=1)
 
 
   }
 
-  it should " be two when NextInt return next two minValue " in  new Checker[Int]  {
-    override def functionToExecute: (Int) => (Int, RandomGenerator) = (a) => buildRandomize(addToMinValue=a).nonNegativeInt
-    test(addMinValue = 2,result=2)
+  it should " be two when NextInt return next two minValue " in    {
+    nonNegative.test(addMinValue = 2,result=2)
   }
 
-  it should "be max when return min sum nexValue is  max " in  new Checker[Int]  {
-    override def functionToExecute: (Int) => (Int, RandomGenerator) = (a) => buildRandomize(addToMinValue=a).nonNegativeInt
-    test(addMinValue = 3,result=3)
+  it should "be max when return min sum nexValue is  max " in   {
+    nonNegative.test(addMinValue = 3,result=3)
   }
 
-  it should "be zero when return min sum nexValue is one upper than max " in  new Checker[Int]  {
-    override def functionToExecute: (Int) => (Int, RandomGenerator) = (a) => buildRandomize(addToMinValue=a).nonNegativeInt
-    test(addMinValue = 4,result=0)
+  it should "be zero when return min sum nexValue is one upper than max " in    {
+    nonNegative.test(addMinValue = 4,result=0)
   }
 
-  it should "be one when return min sum nexValue is two upper than max " in  new Checker[Int]  {
-    override def functionToExecute: (Int) => (Int, RandomGenerator) = (a) => buildRandomize(addToMinValue=a).nonNegativeInt
-    test(addMinValue = 5,result=1)
+  it should "be one when return min sum nexValue is two upper than max " in  {
+    nonNegative.test(addMinValue = 5,result=1)
   }
 
   " We want to implement function that return Double between 0 and 1 and result " should " be zero when return min Value " in  new Checker[Double] {
     override def functionToExecute: (Int) => (Double, RandomGenerator) =  (a)=>buildRandomize(addToMinValue=a).doubleRandom
     test(addMinValue = 0,result = 0)
-
-
   }
-
 
   it should " be 1/6 when return min + 1"  in  new Checker[Double] {
     override def functionToExecute: (Int) => (Double, RandomGenerator) =  (a)=>buildRandomize(addToMinValue=a).doubleRandom
@@ -101,18 +117,17 @@ class RandomGeneratorTest extends FlatSpec with ShouldMatchers{
 
   " We want to implement function that return int double and result " should " be (0,0) when nexInt is Min" in new Checker[(Int,Double)] {
     override def functionToExecute: (Int) => ((Int, Double), RandomGenerator) = (a)=> buildRandomize(addToMinValue=a).intDoubleRandom
-    test(addMinValue = 0,result= (0,0) )
+    test(addMinValue = 0,result= (0,0))
   }
 
   it should " be (1,1/6) when nextInt is Min + 1"  in new Checker[(Int,Double)]  {
     override def functionToExecute: (Int) => ((Int, Double), RandomGenerator) = (a)=> buildRandomize(addToMinValue=a).intDoubleRandom
-    test(addMinValue = 1,result= (1,1/6) )
+    test(addMinValue = 1,result= (1,1/6))
   }
 
   it should " be (2,0) when NextInt is Min + 6" in new Checker[(Int,Double)] {
     override def functionToExecute: (Int) => ((Int, Double), RandomGenerator) = (a)=> buildRandomize(addToMinValue=a).intDoubleRandom
-    test(addMinValue = 6,result= (2,0) )
+    test(addMinValue = 6,result= (2,0))
   }
-
 
 }

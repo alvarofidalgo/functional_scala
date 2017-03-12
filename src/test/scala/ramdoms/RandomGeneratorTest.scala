91df82bc49160f16d89880cacab087bc1279742b
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2017-03-12 13:03:27 +0100

    remove doubleRandom trait

--- src/test/scala/ramdoms/RandomGeneratorTest.scala
+++ src/test/scala/ramdoms/RandomGeneratorTest.scala
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith
 import org.scalatest.FlatSpec
 import org.scalatest.ShouldMatchers
 import org.scalatest.junit.JUnitRunner
-import doubles.{DoubleRandomized, MockGenerator, MyRandomized}
+import doubles.{DoubleRandomized, MockGenerator}
 
 @RunWith(classOf[JUnitRunner])
 class RandomGeneratorTest extends FlatSpec with ShouldMatchers{
@@ -18,19 +18,20 @@ class RandomGeneratorTest extends FlatSpec with ShouldMatchers{
      }
   }
 
-  trait NonNegative extends Constants {
 
-    def testNonNegative(addMinValue:Int,result:Double) =
-      buildRandomize(addToMinValue=addMinValue).nonNegativeInt shouldBe (result,MockGenerator)
+
+  trait Checker[A] extends Constants{
+
+    val noneNegative:(Int)=>(Int,RandomGenerator) = (a) => buildRandomize(addToMinValue=a).nonNegativeInt
+    val doubleRandom:(Int)=>(Double,RandomGenerator) = (a)=>buildRandomize(addToMinValue=a).doubleRandom
+
+
+    def test(addMinValue:Int,result:A,f:(Int)=>(A,RandomGenerator)) = {
+      f(addMinValue) shouldBe (result,MockGenerator)
+    }
 
   }
-
-  trait DoubleRandom  extends Constants {
-
-    def testDoubleRandom(addMinValue:Int,result:Double) =
-      buildRandomize(addToMinValue=addMinValue).doubleRandom shouldBe (result,MockGenerator)
-
-  }
+  
 
   trait  IntDoubleRandom  extends Constants {
 
@@ -39,54 +40,54 @@ class RandomGeneratorTest extends FlatSpec with ShouldMatchers{
   }
 
 
-  "We want to generate Random number and result " should " be Zero when nextValue return MinValue" in  new NonNegative {
-    testNonNegative(addMinValue = 0,result=0)
+  "We want to generate Random number and result " should " be Zero when nextValue return MinValue" in  new Checker[Int] {
+    test(addMinValue = 0,result=0,noneNegative)
 
   }
 
-  it should " be one when NextInt return next MinValue " in  new NonNegative  {
-    testNonNegative(addMinValue = 1,result=1)
+  it should " be one when NextInt return next MinValue " in  new Checker[Int]  {
+    test(addMinValue = 1,result=1,noneNegative)
   }
 
-  it should " be two when NextInt return next two minValue " in  new NonNegative  {
-    testNonNegative(addMinValue = 2,result=2)
+  it should " be two when NextInt return next two minValue " in  new Checker[Int]  {
+    test(addMinValue = 2,result=2,noneNegative)
   }
 
-  it should "be max when return min sum nexValue is  max " in  new NonNegative  {
-    testNonNegative(addMinValue = 3,result=3)
+  it should "be max when return min sum nexValue is  max " in  new Checker[Int]  {
+    test(addMinValue = 3,result=3,noneNegative)
   }
 
-  it should "be zero when return min sum nexValue is one upper than max " in  new NonNegative  {
-    testNonNegative(addMinValue = 4,result=0)
+  it should "be zero when return min sum nexValue is one upper than max " in  new Checker[Int]  {
+    test(addMinValue = 4,result=0,noneNegative)
   }
 
-  it should "be one when return min sum nexValue is two upper than max " in  new NonNegative  {
-    testNonNegative(addMinValue = 5,result=1)
+  it should "be one when return min sum nexValue is two upper than max " in  new Checker[Int]  {
+    test(addMinValue = 5,result=1,noneNegative)
   }
 
-  " We want to implement function that return Double between 0 and 1 and result " should " be zero when return min Value " in  new DoubleRandom {
-    testDoubleRandom(addMinValue = 0,result = 0)
+  " We want to implement function that return Double between 0 and 1 and result " should " be zero when return min Value " in  new Checker[Double] {
+    test(addMinValue = 0,result = 0,doubleRandom)
   }
 
 
-  it should " be 1/6 when return min + 1"  in  new DoubleRandom {
-    testDoubleRandom(addMinValue = 1,result = 1/6)
+  it should " be 1/6 when return min + 1"  in  new Checker[Double] {
+    test(addMinValue = 1,result = 1/6,doubleRandom)
   }
 
-  it should " be 2/6 when return min +2 " in  new DoubleRandom {
-    testDoubleRandom(addMinValue = 2,result = 2/6)
+  it should " be 2/6 when return min +2 " in  new Checker[Double] {
+    test(addMinValue = 2,result = 2/6,doubleRandom)
   }
 
-  it should " be zero when return min + 6 " in  new DoubleRandom {
-    testDoubleRandom(addMinValue = 6,result = 0)
+  it should " be zero when return min + 6 " in new Checker[Double]{
+    test(addMinValue = 6,result = 0,doubleRandom)
   }
 
-  it should " be 1/6 when return min + 7 " in  new DoubleRandom {
-    testDoubleRandom(addMinValue = 7,result = 1/6)
+  it should " be 1/6 when return min + 7 " in  new Checker[Double] {
+    test(addMinValue = 7,result = 1/6,doubleRandom)
   }
 
-  it should " be zero when return min + 12 " in  new DoubleRandom {
-    testDoubleRandom(addMinValue = 12,result = 0)
+  it should " be zero when return min + 12 " in  new Checker[Double]{
+    test(addMinValue = 12,result = 0,doubleRandom)
   }
 
   " We want to implement function that return int double and result " should " be (0,0) when nexInt is Min" in new IntDoubleRandom {

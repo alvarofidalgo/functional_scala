0f8ccf3d5fda34ca966d992b1fa2b8f3401c969d
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2017-04-29 18:09:45 +0200

    refactor state

--- src/test/scala/dispenser/MachineTest.scala
+++ src/test/scala/dispenser/MachineTest.scala
@@ -14,18 +14,18 @@ class MachineTest extends FlatSpec with ShouldMatchers {
 
   it should " be in  PAYMENT with 10 cents when machine was in PAYMENT with 0 cents and we inserted 10 cents " in {
     val lastStatus = Machine(amount =0)
-    Machine.insert(money=10)(lastStatus) shouldBe ((PAYMENT,10),Machine(10))
+    Machine.insert(money=10)(lastStatus) shouldBe (PAYMENT,Machine(10))
   }
 
 
   it should "be in PAYMENT with 20 cents when machine was in Payment with 10 cents and we inserted 10 cents " in {
     val lastStatus = Machine(amount = 10)
-    Machine.insert(money=10)(lastStatus)  shouldBe ((PAYMENT,20),Machine(20))
+    Machine.insert(money=10)(lastStatus)  shouldBe (PAYMENT,Machine(20))
   }
 
 
   it should " be in SELECTION with 40 when machine was in PAYMENT with 30 cents and we inserted 10 cents " in {
     val lastStatus = Machine(amount = 30)
-    Machine.insert(money=10)(lastStatus) shouldBe ((SELECTED,40),Machine(40))
+    Machine.insert(money=10)(lastStatus) shouldBe (SELECTED,Machine(40))
   }
 }

8a226213e21770300cfd0ef99883eba6d326ba7c
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2017-05-19 19:40:27 +0200

    generalize

--- src/main/scala/dispenser/CoffeeMachine.scala
+++ src/main/scala/dispenser/CoffeeMachine.scala
@@ -1,16 +1,38 @@
 package dispenser
 
 
+class CoffeeMachine[A](amount:A,price:A) {
 
-class CoffeeMachine(amount:Int) {
 
-  def insert(amount:Int):CoffeeMachine = {
-    new CoffeeMachine(this.amount + amount)
+
+  def calculateAccAmount(amount: A):A =
+    amount match {
+      case i: Int => (this.amount.asInstanceOf[Int] + i).asInstanceOf[A]
+      case i: Double => (this.amount.asInstanceOf[Double] + i).asInstanceOf[A]
+      case i: String =>   (this.amount.asInstanceOf[String].toInt + i).asInstanceOf[A]
+    }
+
+
+  def calculateMissingAmount(amount: A):Int =
+    amount match {
+      case i: Int => this.price.asInstanceOf[Int] - i
+      case i: Double => (this.price.asInstanceOf[Double] - i).asInstanceOf[Int]
+      case i: String =>   this.price.asInstanceOf[String].toInt - i.toInt
+    }
+
+
+  def insert(amount:A):CoffeeMachine[A] = {
+    val accAmount = calculateAccAmount(amount)
+    new CoffeeMachine[A](accAmount,price)
   }
 
   def select():String = {
 
-    val amountMissing = 40 -amount
-    s"$amountMissing cents is missing"
+    val amountMissingAcc = calculateMissingAmount(amount = this.amount)
+
+    if (amountMissingAcc > 0)
+    s"$amountMissingAcc cents is missing"
+    else
+      "your coffee"
   }
 }
--- src/test/scala/dispenser/CoffeeMachineTest.scala
+++ src/test/scala/dispenser/CoffeeMachineTest.scala
@@ -11,18 +11,25 @@ class CoffeeMachineTest extends FlatSpec with ShouldMatchers {
 
   behavior of " John use coffee machine and result "
 
-  val coffeeMachine = new CoffeeMachine(amount = 0)
+
 
   it should " be '30 cents is missing' when 10 cents was introduced and coffee was selected " in {
-
+    val coffeeMachine = new CoffeeMachine[Int](amount = 0,price = 40)
     val newStateMachine = coffeeMachine.insert(amount=10)
     newStateMachine.select() shouldBe "30 cents is missing"
   }
 
 
   it should " be '20 cents is missing' when 20 cents was introduced and coffee was selected " in {
+    val coffeeMachine = new CoffeeMachine[Double](amount = 0,price = 40)
     val newStateMachine = coffeeMachine.insert(amount=20)
     newStateMachine.select() shouldBe "20 cents is missing"
+  }
+
+  it should " be 'your coffee' when 40 cents was introduced and coffee was selected " in {
+    val coffeeMachine = new CoffeeMachine[String](amount = "0",price = "40")
+    val newStateMachine = coffeeMachine.insert(amount="40")
+    newStateMachine.select() shouldBe "your coffee"
   }
 
 

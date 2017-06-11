6ce784d6d256b124ba85080ed8d5e2030d3f0d6c
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2017-06-11 16:52:40 +0200

    make new process

--- src/main/scala/parallelims/api/ParAPI.scala
+++ src/main/scala/parallelims/api/ParAPI.scala
@@ -1,5 +1,6 @@
 package parallelims.api
 
+import java.util.concurrent.Semaphore
 
 
 object ParAPI {
@@ -11,7 +12,20 @@ object ParAPI {
   def map2[A,B,C](first:Par[A],second:Par[B])(f:(A,B)=>C):Par[C] = unit(f(first.get,second.get))
 
 
-  def fork[A](a: => Par[A]):Par[A] = unit(a.get)
+  def fork[A](a: => Par[A]):Par[A] = {
+    val semaphore = new Semaphore(0)
+    var returned:Par[A] = null
+    val thread = new Thread() {
+
+      override def run(): Unit = {
+        returned = a
+        semaphore.release()
+      }
+    }
+    thread.start()
+    semaphore.acquire()
+    returned
+  }
 
 
 }

c50d8ec0b31520be2357c66da1b14c939b90dcc1
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2017-06-11 13:02:11 +0200

    refactor

--- src/main/scala/parallelims/Calculator.scala
+++ src/main/scala/parallelims/Calculator.scala
@@ -7,8 +7,8 @@ class Calculator {
 
 
   def sum(numbers:Seq[Int]):Int = numbers match{
-    case Nil => ParAPI.unit(0).get
-    case head::Nil =>  ParAPI.unit(head).get
+    case Nil => ParAPI.get(ParAPI.unit(0))
+    case head::Nil =>  ParAPI.get(ParAPI.unit(head))
     case head::tail =>
       val (first, second) =numbers.splitAt(numbers.length / 2)
       ParAPI.get(ParAPI.unit(sum(first))) + ParAPI.get(ParAPI.unit(sum(second)))

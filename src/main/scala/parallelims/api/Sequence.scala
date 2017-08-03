36ea95fac3adb8208a7adb967b015aebbc945785
alvarofidalgo <afidalgo@stratio.com>
2017-08-03 14:55:48 +0200

    first implement parMap

--- src/main/scala/parallelims/api/Sequence.scala
+++ src/main/scala/parallelims/api/Sequence.scala
@@ -6,8 +6,11 @@ import parallelims.types.Types.Par
 object Sequence {
 
 
-  implicit class Sequence[A](sequence: Seq[A]){
-    def parMap[B](f: A => B): Par[Seq[B]] = ???
+  implicit class SequenceOperations[A](sequence: Seq[A]){
+
+    def parMap[B](f: A => B): Par[Seq[B]] = (execution) => execution.submit(new Callable[Seq[B]] {
+      override def call: Seq[B] = Seq.empty[B]
+    })
   }
 
 }

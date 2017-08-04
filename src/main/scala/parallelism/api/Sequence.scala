8e1272b79c3a2e34ed37d99cb61b7c0db1fe9f72
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2017-08-04 21:05:07 +0200

    implement more elements

--- src/main/scala/parallelism/api/Sequence.scala
+++ src/main/scala/parallelism/api/Sequence.scala
@@ -8,8 +8,9 @@ object Sequence {
 
   implicit class SequenceOperations[A](sequence: Seq[A]){
 
-    def parMap[B](f: A => B): Par[Seq[B]] = (execution) => execution.submit(new Callable[Seq[B]] {
-      override def call: Seq[B] = Seq.empty[B]
+    def parMap[B](f: A => B): Par[Seq[B]] = (execution) =>
+      execution.submit(new Callable[Seq[B]] {
+          override def call: Seq[B] = sequence.map(f)
     })
   }
 

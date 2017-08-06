9a81050e64b735843b946b74c74ee198e43200a1
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2017-08-06 18:18:56 +0200

    refactor parMap

--- src/main/scala/parallelism/api/Sequence.scala
+++ src/main/scala/parallelism/api/Sequence.scala
@@ -7,10 +7,11 @@ import parallelism.types.Types.Par
 object Sequence {
 
 
-  def parMap[A,B](entrySequence: Seq[A])(f: A => B): Par[Seq[B]] = (execution) =>
-    execution.submit(new Callable[Seq[B]] {
-      override def call: Seq[B] = entrySequence.map(f)
-    })
+  def parMap[A,B](entrySequence: Seq[A])(f: A => B): Par[Seq[B]] = {
+    import Asynchronous._
+    val list = entrySequence.map((value) => value.asyncF(f))
+    sequence(list)
+  }
 
 
   def sequence[A](entrySequence: Seq[Par[A]]): Par[Seq[A]] = (execution) =>{

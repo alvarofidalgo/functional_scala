72de7c65411abeb8f57f62f93657193c41bde81d
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2017-12-04 21:38:17 +0100

    make adjacentSets

--- src/main/scala/sets/SetsFunctions.scala
+++ src/main/scala/sets/SetsFunctions.scala
@@ -1,23 +1,35 @@
 package sets
 
+
 import scala.annotation.tailrec
 
 
 object SetsFunctions {
 
 
-  implicit class Sets (cad:List[String]) {
+  implicit  class Sets (cad:List[String]) {
 
     @tailrec
     final def setsOf(elements:Int)
                     (implicit result:List[String] = List.empty[String],
                               partial:String="",
-                              next:List[String] =  List.empty[String]):List[String] = (cad,partial.length + 1) match  {
+                              next:List[String] =  List.empty[String]):List[String] = (elements,cad,partial.length + 1) match  {
+      case (1,_,_)=>cad
+      case (_,head::tail,x) if x==1 => new Sets(tail).setsOf(elements)(result,s"$partial$head",tail)
+      case (_,head::tail,x) if x < elements => new Sets(tail).setsOf(elements)(result,s"$partial$head",next)
+      case (_,head::_,x) if x == elements => new Sets(next).setsOf(elements)(result ++ List(s"$partial$head"),"",next)
+      case (_,Nil,x) if x > elements => result ++ List(partial)
+      case (_,_,_) => result
+    }
 
-      case (head::tail,x) if x==1 => new Sets(tail).setsOf(elements)(result,s"$partial$head",tail)
-      case (head::tail,x) if x < elements => new Sets(tail).setsOf(elements)(result,s"$partial$head",next)
-      case (head::_,x) if x == elements =>  new Sets(next).setsOf(elements)(result ++ List(s"$partial$head"),"",next)
-      case (Nil,_) => result
+    @tailrec
+    final def adjacentSets(implicit elements:Int = cad.length,result:List[String]=List.empty[String] ):List[String] =elements match {
+      case 0 =>
+        result
+      case _ =>  {
+        val t= setsOf(elements)( List.empty[String],"",List.empty[String])
+        adjacentSets(elements-1,result ++ t )
+      }
     }
   }
 
--- src/test/scala/sets/StringSetTest.scala
+++ src/test/scala/sets/StringSetTest.scala
@@ -13,6 +13,12 @@ class StringSetTest extends FlatSpec with ShouldMatchers{
   behavior of "We want to implement sets function and result "
 
 
+  it should "be sets of one elements " in  {
+    val entry = List("a","b","c")
+    entry.setsOf(elements= 1)
+  }
+
+
   it should " be one set of three elements when have three elements and like sets of three  " in {
 
    val entry = List("a","b","c")
@@ -38,6 +44,26 @@ class StringSetTest extends FlatSpec with ShouldMatchers{
   }
 
 
+
+
+
   behavior of " we want to implement function of all adjacents sets and result "
+
+
+  it should "set of one element when hace only one element " in {
+    val entry = List("a")
+    entry.adjacentSets shouldBe List("a")
+  }
+
+ it should "set of three when have two element" in {
+    val entry = List("a","b")
+    entry.adjacentSets shouldBe List("ab","a","b")
+  }
+
+
+  it should "set of six when have three element" in {
+    val entry = List("a","b","c")
+    entry.adjacentSets shouldBe List("abc","ab","bc","a","b","c")
+  }
 
 }

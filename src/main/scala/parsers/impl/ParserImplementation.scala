b1945e623c3d403473a0226090366ac72accf6e1
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2017-12-06 17:43:16 +0100

    develop map2 in parser

--- src/main/scala/parsers/impl/ParserImplementation.scala
+++ src/main/scala/parsers/impl/ParserImplementation.scala
@@ -80,8 +80,12 @@ object ParserImplementation extends Parsers[Exception,Parser]{
 
     }
 
-
-
+   def map2[A, B, C](p1: Parser[A], p2: Parser[B])(f: (A, B) => C):Parser[C] = (str)=>{
+     (p1(str) ,p2(str)) match {
+       case (Right(a),Right(b))=>Right(f(a,b))
+       case (_,_)=> Left(new Exception)
+     }
+  }
 }
 
 

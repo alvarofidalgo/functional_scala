dbc2e12363367065d739ca692a30dac03a3eec4c
ALVARO FIDALGO <alvarofidalgo@ALVAROs-MacBook-Pro.local>
2018-02-19 21:41:39 +0100

    implement methods in trait

--- src/main/scala/monoid/Foldable.scala
+++ src/main/scala/monoid/Foldable.scala
@@ -4,26 +4,20 @@ import scala.annotation.tailrec
 
 sealed trait Foldable[A] {
 
-
-  def foldMap[B]( monoid: MonoId[B])(f: A => B)(implicit result:B = monoid.zero): B
-  def foldLeft[B](result:B)(fCombiner:(B,A)=>B)(implicit  monoid: MonoId[B]):B
-
-}
-
-
-case class FoldableSeq[A] (seq:Seq[A]) extends Foldable[A] {
+  def seq:Seq[A]
 
   @tailrec
-  override final def foldMap[B]( monoid: MonoId[B])(f: A => B)(implicit result:B = monoid.zero): B =
-    seq match  {
-      case Nil => result
-      case  head::tail => FoldableSeq(tail).foldMap(monoid)(f)( monoid.op(result,f(head)))
-    }
+  final def foldMap[B]( monoid: MonoId[B])(f: A => B)(implicit result:B = monoid.zero,s:Seq[A] = seq): B =
+  s match  {
+    case Nil => result
+    case  head::tail => foldMap(monoid)(f)( monoid.op(result,f(head)),tail)
+  }
 
-
-  def foldLeft[B](result:B)(fCombiner:(B,A)=>B)(implicit  monoid: MonoId[B]):B = {
+  def foldLeft[B](result:B)(fCombiner:(B,A)=>B)(implicit  monoid: MonoId[B]):B =
 
     foldMap(monoid){(elem) =>fCombiner(result,elem)}
 
-  }
 }
+
+
+case class FoldableSeq[A] (seq:Seq[A]) extends Foldable[A]
